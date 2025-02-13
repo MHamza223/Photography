7caf2b62e60dc752505f758b55ff6cac6b99110e
Muhammad Hamza Asim <muhammad.asim-2@student.manchester.ac.uk>
2025-02-13 11:21:20 +0000

    image changes

--- _site/Weddingscript.js
+++ _site/Weddingscript.js
@@ -6,44 +6,7 @@ const closeBtn = document.querySelector('.close');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 
-let currentImageIndex;
-document.addEventListener("DOMContentLoaded", () => {
-  const portfolioItems = document.querySelectorAll('.album-gallery');
 
-  if (portfolioItems.length === 0) return; // Prevent errors if no elements are found
-
-  // Create IntersectionObserver
-  const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-          if (entry.isIntersecting) {
-              entry.target.classList.add('visible');
-              observer.unobserve(entry.target); // Stop observing after it's visible
-          }
-      });
-  }, {
-      threshold: 0.25
-  });
-
-  // Observe each portfolio item
-  portfolioItems.forEach(item => {
-      observer.observe(item);
-  });
-
-  // Function to check if an element is in the viewport
-  function isInViewport(el) {
-      const rect = el.getBoundingClientRect();
-      return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
-  }
-
-  // Ensure images that are already in the viewport on load become visible
-  window.addEventListener("load", () => {
-      portfolioItems.forEach(item => {
-          if (isInViewport(item)) {
-              item.classList.add("visible");
-          }
-      });
-  });
-});
 
     
     
